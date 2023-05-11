e679e6716ae7fe8da6f2fb8cca735c76181e2bd8
AlexanderSimakov <simalexander03@gmail.com>
2023-05-11 14:32:11 +0300

    inis lab 5

---
+++ inis5/input.js
@@ -0,0 +1,92 @@
+const blocks = document.querySelectorAll('.target');
+
+const startPos = {
+  y: '0px',
+  x: '0px',
+  block: null,
+}
+
+blocks.forEach((el) => {
+  el.addEventListener('dblclick', (ev) => {
+    el.classList.add('active');
+    moveBlock(ev, el);
+    startPos.block = el;
+    el.onclick = () => {
+      el.classList.remove('active');
+    }
+  })
+  el.addEventListener('mousedown', (ev) => {
+    startPos.block = el;
+    moveBlock(ev, el);
+  })
+  el.addEventListener('touchstart', (ev) => {
+    moveBlock(ev.touches[0], el);
+    startPos.block = el;
+  });
+  el.addEventListener('mouseup', (ev) => {
+    startPos.block = null;
+  });
+})
+
+document.addEventListener('touchstart', (ev) => {
+  let target = ev.target.closest('.target');
+  if (!target) return;
+  
+  if (!touchtime) {
+    touchtime = Date.now();
+  } else {
+    if (Date.now() - touchtime < 200) {
+      startPos.block = target;
+      target.classList.add('active');
+    } else {
+      touchtime = Date.now();
+    }
+  }
+});
+
+document.addEventListener('mouseup', (ev) => {
+  startPos.block = null; 
+});
+
+function resetPos() {
+  startPos.block.style.left = startPos.x;
+  startPos.block.style.top = startPos.y;
+  startPos.block.classList.remove('active');
+  document.onmousemove = null;
+  document.ontouchmove = null;
+  startPos.block.onmouseup = null;
+  startPos.style.zIndex = 1;
+}
+
+document.addEventListener('touchmove', ev => {
+  if (ev.touches.length < 2) {
+    moveAt(ev.touches[0], startPos.block);
+  }
+})
+
+document.addEventListener('keydown', (ev) => {
+  if (ev.key === 'Escape') {
+    resetPos();
+  }
+});
+
+
+function moveBlock(e, el) {
+  moveAt(e, el);
+  el.style.zIndex = 2;
+  [startPos.y, startPos.x] = [el.style.top, el.style.left];
+  
+  document.onmousemove = function(e) {
+    moveAt(e, el);
+  }
+
+  el.onmouseup = function() {
+    document.onmousemove = null;
+    el.onmouseup = null;
+  }
+}
+
+function moveAt(e, el) {
+    el.style.left = e.pageX - el.offsetWidth / 2 + 'px';
+    el.style.top = e.pageY - el.offsetHeight / 2 + 'px';
+  }
